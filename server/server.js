f604d93ca1f5e85989654c6a00c68e8a2098e5a0
onkar dudhale <dudhaleonkar@gmail.com>
2026-04-10 22:15:52 +0530

    changes in frontend url

--- server/server.js
+++ server/server.js
@@ -18,7 +18,7 @@ const port = 3000;
 // middleware
 app.use(cookieParser());
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: `${process.env.FRONTEND_URL}` || "http://localhost:5173",
     credentials: true
 }));
 
